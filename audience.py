c4f42eb3e9a79633fd83b2b5b7c2433cb773abb7
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2024-05-06 13:45:30 +0600

    initial commit

---
+++ audience.py
@@ -0,0 +1,50 @@
+import cv2
+import socket
+import pickle
+import threading
+from queue import Queue
+
+def receive_frames(s, frame_queue, stop_event):
+    while not stop_event.is_set():
+        try:
+            data, _ = s.recvfrom(1000000)
+            if data == b'quit':  # Check for 'quit' signal
+                stop_event.set()
+                break
+            data = pickle.loads(data)
+            frame_queue.put(data)
+        except (ConnectionResetError, OSError):
+            print("Connection reset or error occurred. Exiting...")
+            stop_event.set()
+            break
+
+def display_frames(frame_queue, window_name, stop_event):
+    while not stop_event.is_set():
+        if not frame_queue.empty():
+            data = frame_queue.get()
+            img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+            cv2.imshow(window_name, img)
+            cv2.waitKey(1)
+        else:
+            continue
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = socket.gethostbyname(socket.gethostname())
+    port = 6666
+    s.bind((ip, port))
+
+    frame_queue = Queue()
+    stop_event = threading.Event()
+
+    receive_thread = threading.Thread(target=receive_frames, args=(s, frame_queue, stop_event))
+    receive_thread.start()
+
+    display_thread = threading.Thread(target=display_frames, args=(frame_queue, 'Audience', stop_event))
+    display_thread.start()
+
+    display_thread.join()
+    receive_thread.join()
+
+    cv2.destroyAllWindows()
+    s.close()
---
+++ streamer.py
@@ -0,0 +1,63 @@
+import cv2
+import socket
+import pickle
+import threading
+from queue import Queue
+
+class VideoStreamer:
+    def __init__(self, server_ip, server_port):
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, 640)
+        self.cap.set(4, 480)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.stop_event = threading.Event()
+        self.frame_queue = Queue()
+        self.video_stream_thread = threading.Thread(target=self.video_stream)
+        self.display_thread = threading.Thread(target=self.display_frames)
+
+    def video_stream(self):
+        while not self.stop_event.is_set() and self.cap.isOpened():
+            ret, img = self.cap.read()
+            if not ret:
+                print("Error reading frame")
+                continue
+            print('Reading image')
+            self.frame_queue.put(img)
+            ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+            print('Encoding image')
+            x_as_bytes = pickle.dumps(buffer)
+            try:
+                self.s.sendto((x_as_bytes), (self.server_ip, self.server_port))
+            except (ConnectionError, OSError) as e:
+                print(f'Error: {e}')
+                break
+
+    def display_frames(self):
+        while not self.stop_event.is_set():
+            if not self.frame_queue.empty():
+                frame = self.frame_queue.get()
+                cv2.imshow('Streamer', frame)
+                if cv2.waitKey(1) == ord('q'):
+                    self.stop_event.set()
+                    self.s.sendto(b'quit', (self.server_ip, self.server_port))  # Send 'quit' signal
+
+    def start(self):
+        self.video_stream_thread.start()
+        self.display_thread.start()
+
+    def stop(self):
+        self.stop_event.set()
+        self.video_stream_thread.join()
+        self.display_thread.join()
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    server_ip = socket.gethostbyname(socket.gethostname())
+    server_port = 6666
+
+    streamer = VideoStreamer(server_ip, server_port)
+    streamer.start()
