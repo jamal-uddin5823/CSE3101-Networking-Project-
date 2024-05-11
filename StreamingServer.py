8df15c6176b6ac6fb2fd17840616069c37252c5b
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2024-05-11 16:58:47 +0600

    just server chat left

--- StreamingServer.py
+++ StreamingServer.py
@@ -321,16 +321,3 @@ class StreamingServer:
         exit()
 
 
-# Usage example
-streaming_server = StreamingServer(socket.gethostbyname(socket.gethostname()), 8000, 8001,9000)
-streaming_server.start()
-
-if cv2.waitKey(25) == ord('q'):
-    print("Quitting... from here")
-    for client in streaming_server.video_clients:
-        streaming_server.video_socket.sendto(b'quit', client)
-        streaming_server.audio_socket.sendto(b'quit', client)
-    streaming_server.stop()
-
-# Wait for Ctrl+C or some other condition to stop
-# streaming_server.stop()
--- streamer.py
+++ streamer.py
@@ -1,16 +1,18 @@
-from VideoStreamer import VideoStreamer
-from AudioStreamer import AudioStreamer
+from StreamingServer import StreamingServer
 import socket
 import cv2
 
-if __name__ == "__main__":
-    server_ip = socket.gethostbyname(socket.gethostname())
-    server_video_port = 6666
-    streamer = VideoStreamer(server_ip, server_video_port)
-    streamer.start()
- 
-    if cv2.waitKey(25) == ord('q'):
-        print("Quitting... from here")
-        for client in streamer.clients:
-            streamer.s.sendto(b'quit', client)
-        streamer.stop()
+
+# Usage example
+streaming_server = StreamingServer(socket.gethostbyname(socket.gethostname()), 8000, 8001,9000)
+streaming_server.start()
+
+if cv2.waitKey(25) == ord('q'):
+    print("Quitting... from here")
+    for client in streaming_server.video_clients:
+        streaming_server.video_socket.sendto(b'quit', client)
+        streaming_server.audio_socket.sendto(b'quit', client)
+    streaming_server.stop()
+
+# Wait for Ctrl+C or some other condition to stop
+# streaming_server.stop()
